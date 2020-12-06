480b09508ca13850b429b2589df0f8dd00abf271
Davi M <71362773+Zerufora@users.noreply.github.com>
2020-12-06 10:28:39 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+const { Client } = require('discord.js');
+const app = new Client();
+
+app.on('ready', () => {
+    console.log(`Estou logado no corpo de ${app.user.tag}.`);
+    app.user.setActivity('Bom dia!', { type: "LISTENING" });
+});
+
+app.on('message', (m) => {
+    if (m.author.bot) return;
+    if (m.channel.type === "dm") return;
+
+    if (!m.content.toLowerCase().includes('bom') || !m.content.toLowerCase().includes('dia'))  {
+        m.delete();
+        m.author.send('Apenas mensagens de bom dia no #xet-de-bom-dia');
+    }
+});
+
+app.login(process.env.AUTH_TOKEN);
